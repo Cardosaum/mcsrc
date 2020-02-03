64d31a2cdb49a5feb881e67f28e03a50a4d15541
Cardosaum <mcardosos99@gmail.com>
2020-02-03 18:38:31 -0300

    finished books.py (?)

--- scripts/books.py
+++ scripts/books.py
@@ -12,7 +12,7 @@ import csv
 
 # class Book(bookFilteredDataFrame, book):
 # 	"""Take a unique Book in a pd.DataFrame and perform stats fill for attributes like 'currentPage', etc."""
-# 	def __init__(self, name, totalPages, currenPage):
+# 	def __init__(self, name, totalPages, currentPage):
 # 		self.name = book
 		
 def getBooksNames(booksDataFrame):
@@ -74,27 +74,93 @@ def askCurrentPage(bookDataFrame):
 			else:
 				page = int(page)
 				break
-		return page
+		# check if page is greater than currentPage (Maybe user inserted wrong value?)
+		currentPage = getBookStats(bookDataFrame)['currentPage']
+		if page <= currentPage:
+			print('Are you sure you made progress in this book?')
+			print(f'· Current Page: {currentPage}')
+			print(f'· Your input: {page}')
+			print()
+			print('Do you realy want to update with your input page?')
+			print(f'Your progress will be: `{currentPage} - {page} = {currentPage-page}`')
+			print('Proceed? [y/N]')
+			while True:
+				proceed = input('> ').strip().lower()
+				if proceed not in ['y', 'n', '']:
+					print('Please, type only:\n"Y" for "YES"\n"N" for "NO"\n(Note: empty input will be considered as "NO"\n')
+				elif proceed in ['n', '']:
+					print(f"Okay, I'll disconsider your input ({page}), and mantain {currentPage}")
+					break
+				else:
+					print('Okay, I\'ll update with your input')
+					break
+					return page
+		else:
+			return page
 
 def performBooksFileUpdate(bookDataFrame, currentPage, books_file):
 	pd.options.mode.chained_assignment = None
 	rowToUpdate = bookDataFrame.iloc[-1]
 	rowToUpdate.currentPage = currentPage
+	rowToUpdate.time = datetime.datetime.now().strftime('%s')
 	rowToUpdate = list(rowToUpdate)
-	# rowToUpdate.append('\n')
 	with open(books_file, 'a', newline='') as books:
 		b = csv.writer(books, lineterminator='\n')
 		b.writerow(rowToUpdate)
 
-def performBookUpdateMarkdown(bookDataFrame):
-	pass
+def performBookUpdateMarkdown(bookDataFrame, books_markdown, book, bookStats):
+	with in_place.InPlace(books_markdown, backup_ext='.bak') as f:
+	# Regex to find lines that correspond to book `name`
+		nRe = re.compile(book)
 
-def mainBooks(books_file):
+		for line in f:
+			nMo = nRe.search(line)
+
+			# if line contain book `name`:
+			if nMo:
+
+				# we need to skip lines that contain status for book.
+				# see layout of file beeing parsed: https://github.com/Cardosaum/bioinformatics_pathway/blob/master/mcs_self_paced.md
+
+				statusFile = re.compile(r'\sOK')
+				sfMo = statusFile.search(line)
+				if not sfMo:
+					# print(book)
+					# print(line)
+					# print()
+
+					# Group 1 of this regex return the current page
+					updateCurrentPageRe = re.compile(r'''
+														\/week\s*\| # capture /week 
+														\s*(\d+)      # capture  <digits>
+														\s*pages?\s*\|   # capture page OR pages
+														\s*([\d.])+%+\s* # capture percentage
+													''', re.VERBOSE)
+					updateCurrentPageMo = updateCurrentPageRe.search(line)
+					# print('-'*20)
+					# print(updateCurrentPageRe.sub(f'''/week | {str(bookStats['currentPage'])} pages''', line))
+
+					# Calculate percentage of progress
+					try:
+						# print(infos)
+						# print(updateCurrentPageMo)
+						calculatePercent = (bookStats["currentPage"]/bookStats["totalPages"])*100
+					except ZeroDivisionError:
+						calculatePercent = 0
+					# print(updateCurrentPageRe.sub(f'''/week | {str(bookStats["currentPage"])} pages | {float(calculatePercent):.2f}%\n''', line))
+					line = updateCurrentPageRe.sub(f'''/week | {str(bookStats["currentPage"])} pages | {float(calculatePercent):.2f}%\n''', line) 
+					# print('='*20)
+
+			# Performs write action
+			f.write(line)
+
+
+def mainBooks(books_file, books_markdown):
 	
 	booksDataFrame = pd.read_csv(books_file)
 	books_names = getBooksNames(booksDataFrame)
 	
-	for book in books_names[0:1]:
+	for book in books_names:
 		# TODO: Get unique book DataFrame
 		bookDataFrame =  getBookDataFrame(booksDataFrame, book)
 		# TODO: show book stats
@@ -102,22 +168,25 @@ def mainBooks(books_file):
 		printBookStats(bookStats)
 
 		# TODO: ask current page
-		# currentPage = askCurrentPage(bookDataFrame)
+		currentPage = askCurrentPage(bookDataFrame)
 
 		# TODO: ask if want to corret some info
 
 		# TODO: show aproximatly time remaining
 		# TODO: perform update in log file
-		currentPage = 10
-		performBooksFileUpdate(bookDataFrame, currentPage, books_file)
 		
-		# if currentPage:
-		# 	print('+'*70)
-		# 	performBookUpdateDataFrame(bookDataFrame, currentPage)
+		currentPage = 10
+
+		if currentPage:
+			# print('log file updated'.upper().center(50, '+'))
+			performBooksFileUpdate(bookDataFrame, currentPage, books_file)
+			bookStats = getBookStats(bookDataFrame)
+			performBookUpdateMarkdown(bookDataFrame, books_markdown, book, bookStats)
 
 
 if __name__ == '__main__':
 
 	# TODO: remove `books_file` from here, and catch tthis info parsing `sys.arg` or something similar
 	books_file = os.path.join('..', 'data', 'books_logs.csv')
-	mainBooks(books_file)
+	books_markdown = os.path.join('..', '..', '..', '..', 'bioinformatics_pathway', 'mcs_self_paced.md')
+	mainBooks(books_file, books_markdown)
